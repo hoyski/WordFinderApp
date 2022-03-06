a9fffa90e6596d8b8b496ff1f92860de62ffbd8f
Dave Hoy <hoyski@gmail.com>
2022-03-05 19:21:35 -0500

    Made deployment changes

--- wordfinder-vue/src/services/WordFinderService.js
+++ wordfinder-vue/src/services/WordFinderService.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 // Use this URL when developing locally and running Vue separately from the service
-axios.defaults.baseURL = 'http://localhost:5000';
+//axios.defaults.baseURL = 'http://localhost:5000';
 
 // Use this URL when bundling the Vue into the application WAR file
-//axios.defaults.baseURL = '.';
+axios.defaults.baseURL = '.';
 
 export default {
     getWords(characters, pattern, indexOfFirst, maxToReturn) {
